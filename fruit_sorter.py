080cfd2dce1ac8476b69eb57949c04b0a43b7bec
andrew21-meet <andrew21@meet.mit.edu>
2019-07-24 14:29:12 +0300

    Stopping here

---
+++ fruit_sorter.py
@@ -0,0 +1,11 @@
+fruit = input("insert a name of a fruit ")
+fruit = fruit.capitalize()
+if(fruit == "Apples"):
+    print ("Apples in bin 1")
+elif(fruit =="Oranges"):
+    print ("Oranges in bin 2")
+elif(fruit=="Bananas"):
+    print ("Bananas in bin 3")
+else:
+    print ("I am sorry!, I don't recognize your fruit")
+           
